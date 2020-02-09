330858cba5602e0fd3357c23845f993032031f86
kudeh <kene.udeh@gmail.com>
2020-02-09 16:02:09 -0500

    updated sequence of table creation to match FKey dependencies

--- 1-Cloud-Data-Warehouses/Projects/sparkify-dwh/sql_queries.py
+++ 1-Cloud-Data-Warehouses/Projects/sparkify-dwh/sql_queries.py
@@ -11,10 +11,10 @@ ARN = config.get('IAM_ROLE', 'ARN')
 
 staging_events_table_drop = "DROP TABLE IF EXISTS events_staging"
 staging_songs_table_drop = "DROP TABLE IF EXISTS songs_staging"
-songplay_table_drop = "DROP TABLE IF EXISTS songplays"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays CASCADE"
+user_table_drop = "DROP TABLE IF EXISTS users CASCADE"
+song_table_drop = "DROP TABLE IF EXISTS songs CASCADE"
+artist_table_drop = "DROP TABLE IF EXISTS artists CASCADE"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS songs_staging
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays 
 (
-    songplay_id IDENTITY(0,1) DISTKEY SORTKEY PRIMARY KEY,
+    songplay_id INT IDENTITY(0,1) DISTKEY SORTKEY PRIMARY KEY,
     start_time TIMESTAMP,
     user_id INT REFERENCES users(user_id),
     level VARCHAR,
@@ -155,7 +155,7 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
