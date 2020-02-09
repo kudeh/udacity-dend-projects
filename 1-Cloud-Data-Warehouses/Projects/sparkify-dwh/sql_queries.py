3aa97457d50540188a5d6cc242969211f1441246
kudeh <kene.udeh@gmail.com>
2020-02-09 14:21:26 -0500

    added created & drop table queries

--- 1-Cloud-Data-Warehouses/Projects/sparkify-dwh/sql_queries.py
+++ 1-Cloud-Data-Warehouses/Projects/sparkify-dwh/sql_queries.py
@@ -7,35 +7,115 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF NOT EXISTS events_staging"
+staging_songs_table_drop = "DROP TABLE IF NOT EXISTS songs_staging"
+songplay_table_drop = "DROP TABLE IF NOT EXISTS songplays"
+user_table_drop = "DROP TABLE IF NOT EXISTS users"
+song_table_drop = "DROP TABLE IF NOT EXISTS songs"
+artist_table_drop = "DROP TABLE IF NOT EXISTS artists"
+time_table_drop = "DROP TABLE IF NOT EXISTS time"
 
 # CREATE TABLES
 
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS events_staging
+(
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
+    gender CHAR(1),
+    itemInSession INT,
+    lastName VARCHAR,
+    length NUMERIC,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration NUMERIC,
+    sessionId INT,
+    song VARCHAR,
+    status INT,
+    ts TIMESTAMP,
+    userAgent VARCHAR,
+    userId INT
+)
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs_staging 
+(
+    num_songs INT,
+    artist_id VARCHAR,
+    artist_latitude NUMERIC,
+    artist_longitude NUMERIC,
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR,
+    duration NUMERIC,
+    year INT
+)
 """)
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays 
+(
+    songplay_id IDENTITY(0,1) PRIMARY KEY,
+    start_time TIMESTAMP,
+    user_id INT REFERENCES users(user_id),
+    level VARCHAR,
+    song_id VARCHAR REFERENCES songs(song_id),
+    artist_id VARCHAR REFERENCES artists(artist_id),
+    session_id INT,
+    location VARCHAR,
+    user_agent VARCHAR
+)
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users 
+(
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    gender CHAR(1),
+    level VARCHAR
+)
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs 
+(
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR,
+    artist_id VARCHAR,
+    year INT,
+    duration NUMERIC
+)
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists 
+(
+    artist_id VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    location VARCHAR,
+    latitude NUMERIC,
+    longitude NUMERIC
+)
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time 
+(
+    start_time TIMESTAMP,
+    hour INT,
+    day INT,
+    week INT,
+    month INT,
+    year INT,
+    weekday INT
+)
 """)
 
 # STAGING TABLES
