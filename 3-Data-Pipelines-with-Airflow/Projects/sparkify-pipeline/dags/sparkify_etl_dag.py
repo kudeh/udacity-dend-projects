c722beade7bd687f197f6214f1564396b4c9a7a4
kudeh <kene.udeh@gmail.com>
2020-03-04 21:03:14 -0500

    sparkify-pipeline project init commit

---
+++ 3-Data-Pipelines-with-Airflow/Projects/sparkify-pipeline/dags/sparkify_etl_dag.py
@@ -0,0 +1,88 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'kene',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 12),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+}
+
+dag = DAG('sparkify_etl_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table='staging_events',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity-dend',
+    s3_key='log_data',
+    json_paths='log_json_path.json',
+    use_partitioned=True,
+    partition_template='{execution_date.year}/{execution_date.month}'
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    table='staging_songs',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='udacity-dend',
+    s3_key='song_data',
+    use_partitioned=True,
+    partition_template='{execution_date.year}/{execution_date.month}'
+)
+
+# load_songplays_table = LoadFactOperator(
+#     task_id='Load_songplays_fact_table',
+#     dag=dag
+# )
+
+# load_user_dimension_table = LoadDimensionOperator(
+#     task_id='Load_user_dim_table',
+#     dag=dag
+# )
+
+# load_song_dimension_table = LoadDimensionOperator(
+#     task_id='Load_song_dim_table',
+#     dag=dag
+# )
+
+# load_artist_dimension_table = LoadDimensionOperator(
+#     task_id='Load_artist_dim_table',
+#     dag=dag
+# )
+
+# load_time_dimension_table = LoadDimensionOperator(
+#     task_id='Load_time_dim_table',
+#     dag=dag
+# )
+
+# run_quality_checks = DataQualityOperator(
+#     task_id='Run_data_quality_checks',
+#     dag=dag
+# )
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+[stage_events_to_redshift, stage_songs_to_redshift] >> end_operator
---
+++ 3-Data-Pipelines-with-Airflow/Projects/sparkify-pipeline/plugins/operators/stage_redshift.py
@@ -0,0 +1,68 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    template_fields = ("partition_template",)
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        FORMAT AS JSON {}
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 json_paths="",
+                 use_partitioned=False,
+                 partition_template="",
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.table = table,
+        self.redshift_conn_id = redshift_conn_id,
+        self.aws_credentials_id = aws_credentials_id,
+        self.s3_bucket = s3_bucket,
+        self.s3_key = s3_key,
+        self.json_paths = json_paths,
+        self.use_partitioned = use_partitioned,
+        self.partition_template = partition_template
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info('Clearing data from {}'.format(self.table))
+        redshift.run('DELETE FROM {}'.format(self.table))
+        
+       
+        s3_path = 's3://{}/{}'.format(self.s3_bucket, self.s3_key)
+        if self.use_partitioned:
+            rendered_partition = self.partition_template.format(**context)
+            s3_path = '{}/{}'.format(s3_path, rendered_partition)
+             
+        if self.json_paths:
+            json_paths = 's3://{}/{}'.format(self.s3_bucket, self.json_paths)
+        else:
+            json_paths = 'auto'
+            
+        self.log.info('Coping data from {} to {} on Redshift'.format(s3_path, table))
+  
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            json_paths
+        )
+        redshift.run(formatted_sql)
+        
